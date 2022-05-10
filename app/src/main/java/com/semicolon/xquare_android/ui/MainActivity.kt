d17c941050e1c80a732cf83e3abdaf68b8174b8e
Jaewon <turitus@naver.com>
2022-05-10 14:05:15 +0900

    ⚰️:: unusable composable

---
+++ app/src/main/java/com/semicolon/xquare_android/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.semicolon.xquare_android.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.semicolon.xquare_android.ui.theme.XquareandroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            XquareandroidTheme {
+
+            }
+        }
+    }
+}
