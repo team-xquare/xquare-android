b93511132cdcd2e7bdde7e01b189237d18ab00e0
Jaewon <turitus@naver.com>
2022-05-31 13:58:42 +0900

    ⚡️:: meal repository impl

---
+++ data/src/main/java/com/xquare/data/repository/meal/MealRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package com.xquare.data.repository.meal
+
+import com.xquare.domain.entity.meal.MealEntity
+import com.xquare.domain.repository.meal.MealRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class MealRepositoryImpl @Inject constructor(
+    
+): MealRepository {
+
+    override suspend fun fetchTodayMeal(): Flow<MealEntity> {
+        TODO("Not yet implemented")
+    }
+}
