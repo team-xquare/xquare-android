74a2a4ea61bc1d2158ef7b62ec6e25cc3a0fc2a4
shw0471 <shw0471@naver.com>
2022-06-02 15:48:13 +0900

    ♻️ :: variable name

--- data/src/main/java/com/xquare/data/remote/response/profile/ProfileImageResponse.kt
+++ data/src/main/java/com/xquare/data/remote/response/profile/ProfileImageResponse.kt
@@ -3,5 +3,5 @@ package com.xquare.data.remote.response.profile
 import com.google.gson.annotations.SerializedName
 
 data class ProfileImageResponse(
-    @SerializedName("image_url") val image_url: String
+    @SerializedName("image_url") val imageUrl: String
 )
