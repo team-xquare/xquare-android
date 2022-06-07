d4d2c981d1a62d707bcf73145c1fc412826ec8b1
shw0471 <shw0471@naver.com>
2022-06-07 16:00:57 +0900

    ⚡️ :: update authorization interceptor

---
+++ data/src/main/java/com/xquare/data/interceptor/AuthorizationInterceptor.kt
@@ -0,0 +1,64 @@
+package com.xquare.data.interceptor
+
+import com.google.gson.Gson
+import com.xquare.data.local.preference.AuthPreference
+import com.xquare.data.remote.response.auth.TokenResponse
+import com.xquare.domain.exception.NeedLoginException
+import kotlinx.coroutines.runBlocking
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.ZoneId
+import javax.inject.Inject
+
+class AuthorizationInterceptor @Inject constructor(
+    private val authPreference: AuthPreference
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val path = request.url.encodedPath
+        val method = request.method
+        val ignorePath = listOf(
+            "/users/login"
+        )
+        if (ignorePath.contains(path)) return chain.proceed(request)
+        else if (path == "/users" && method == "POST") return chain.proceed(request)
+
+        val expiredAt = runBlocking { authPreference.fetchExpirationAt() }
+        val currentTime = LocalDateTime.now(ZoneId.systemDefault())
+
+        if (expiredAt.isBefore(currentTime)) {
+            val client = OkHttpClient()
+            val refreshToken = runBlocking { authPreference.fetchRefreshToken() }
+
+            val tokenRefreshRequest = Request.Builder()
+                .url("https://server.walkhub.co.kr/users/token")
+                .put("".toRequestBody("application/json".toMediaTypeOrNull()))
+                .addHeader("Refresh-Token", "Bearer $refreshToken")
+                .build()
+            val response = client.newCall(tokenRefreshRequest).execute()
+
+            if (response.isSuccessful) {
+                val token = Gson().fromJson(
+                    response.body!!.toString(),
+                    TokenResponse::class.java
+                )
+                runBlocking {
+                    authPreference.saveAccessToken(token.accessToken)
+                    authPreference.saveRefreshToken(token.refreshToken)
+                    authPreference.saveExpirationAt(LocalDateTime.parse(token.expirationAt))
+                }
+            } else throw NeedLoginException()
+        }
+
+        val accessToken = runBlocking { authPreference.fetchAccessToken() }
+        return chain.proceed(
+            request.newBuilder().addHeader(
+                "Authorization",
+                "Bearer $accessToken"
+            ).build()
+        )
+    }
+}
