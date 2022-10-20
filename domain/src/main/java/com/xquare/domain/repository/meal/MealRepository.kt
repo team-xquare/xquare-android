b748cd86ab77510995b09c780ab921061ee75d7d
shw0471 <shw0471@naver.com>
2022-10-20 11:07:58 +0900

    ⚡️ :: update meal repository

--- domain/src/main/java/com/xquare/domain/repository/meal/MealRepository.kt
+++ domain/src/main/java/com/xquare/domain/repository/meal/MealRepository.kt
@@ -1,9 +1,12 @@
 package com.xquare.domain.repository.meal
 
+import com.xquare.domain.entity.meal.AllMealEntity
 import com.xquare.domain.entity.meal.MealEntity
 import kotlinx.coroutines.flow.Flow
 
 interface MealRepository {
 
     suspend fun fetchTodayMeal(): Flow<MealEntity>
+
+    suspend fun fetchAllMeal(): Flow<AllMealEntity>
 }
