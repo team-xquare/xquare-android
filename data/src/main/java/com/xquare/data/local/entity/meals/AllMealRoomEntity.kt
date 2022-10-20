d37ac9a84e67c9249c866177a2b50b4ba285d2af
shw0471 <shw0471@naver.com>
2022-10-20 11:05:31 +0900

    ⚡️ :: update mapping function

--- data/src/main/java/com/xquare/data/local/entity/meals/AllMealRoomEntity.kt
+++ data/src/main/java/com/xquare/data/local/entity/meals/AllMealRoomEntity.kt
@@ -5,6 +5,8 @@ import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.xquare.domain.entity.meal.AllMealEntity
+import org.threeten.bp.LocalDateTime
 
 @Entity(primaryKeys = ["year", "month"])
 data class AllMealRoomEntity(
@@ -37,3 +39,37 @@ class MealWithDateListTypeConverter(
     fun fromList(value: List<AllMealRoomEntity.MealWithDateRoomEntity>): String =
         gson.toJson(value)
 }
+
+fun AllMealRoomEntity.toEntity() =
+    AllMealEntity(
+        meals = meals.map { it.toEntity() }
+    )
+
+fun AllMealRoomEntity.MealWithDateRoomEntity.toEntity() =
+    AllMealEntity.MealWithDateEntity(
+        date = LocalDateTime.parse(date),
+        breakfast = breakfast,
+        lunch = lunch,
+        dinner = dinner,
+        caloriesOfBreakfast = caloriesOfBreakfast,
+        caloriesOfLunch = caloriesOfLunch,
+        caloriesOfDinner = caloriesOfDinner
+    )
+
+fun AllMealEntity.toRoomEntity(year: Int, month: Int) =
+    AllMealRoomEntity(
+        year = year,
+        month = month,
+        meals = meals.map { it.toRoomEntity() }
+    )
+
+fun AllMealEntity.MealWithDateEntity.toRoomEntity() =
+    AllMealRoomEntity.MealWithDateRoomEntity(
+        date = date.toString(),
+        breakfast = breakfast,
+        lunch = lunch,
+        dinner = dinner,
+        caloriesOfBreakfast = caloriesOfBreakfast,
+        caloriesOfLunch = caloriesOfLunch,
+        caloriesOfDinner = caloriesOfDinner
+    )
