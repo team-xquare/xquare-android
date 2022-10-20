bb80350816fdc7efcb350074821489991c918a85
shw0471 <shw0471@naver.com>
2022-10-20 18:57:55 +0900

    ⚡️ :: day of week to korean extend function

--- app/src/main/java/com/xquare/xquare_android/util/Utils.kt
+++ app/src/main/java/com/xquare/xquare_android/util/Utils.kt
@@ -2,7 +2,19 @@ package com.xquare.xquare_android.util
 
 import android.content.Context
 import android.widget.Toast
+import org.threeten.bp.DayOfWeek
 
 fun makeToast(context: Context, text: String) {
     Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
 }
+
+fun DayOfWeek.toKorean() =
+    when(this) {
+        DayOfWeek.MONDAY -> "월"
+        DayOfWeek.TUESDAY -> "화"
+        DayOfWeek.WEDNESDAY -> "수"
+        DayOfWeek.THURSDAY -> "목"
+        DayOfWeek.FRIDAY -> "금"
+        DayOfWeek.SATURDAY -> "토"
+        DayOfWeek.SUNDAY -> "일"
+    }
