9132ff37b922663f50bd99619809c82960a06167
shw0471 <shw0471@naver.com>
2022-10-20 18:58:21 +0900

    ⚡️ :: update all meal view model

---
+++ app/src/main/java/com/xquare/xquare_android/feature/allmeal/AllMealViewModel.kt
@@ -0,0 +1,23 @@
+package com.xquare.xquare_android.feature.allmeal
+
+import com.xquare.domain.entity.meal.AllMealEntity
+import com.xquare.domain.usecase.meal.FetchAllMealUseCase
+import com.xquare.xquare_android.base.BaseViewModel
+import javax.inject.Inject
+
+class AllMealViewModel @Inject constructor(
+    private val fetchAllMealUseCase: FetchAllMealUseCase
+) : BaseViewModel<AllMealViewModel.Event>() {
+
+    fun fetchAllMeal() =
+        execute(
+            job = { fetchAllMealUseCase.execute(Unit) },
+            onSuccess = { it.collect { allMeal -> emitEvent(Event.Success(allMeal)) } },
+            onFailure = { emitEvent(Event.Failure) }
+        )
+
+    sealed class Event {
+        data class Success(val data: AllMealEntity) : Event()
+        object Failure : Event()
+    }
+}
