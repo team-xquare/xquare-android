8fa47c9834cd441e506c32719bb1f64138ea63ac
shw0471 <shw0471@naver.com>
2022-10-21 14:26:24 +0900

    ⚡️ :: update webview related business logic

---
+++ data/src/main/java/com/xquare/data/repository/WebViewRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.xquare.data.repository
+
+import com.xquare.data.local.datasource.AuthLocalDataSource
+import com.xquare.data.remote.datasource.AuthRemoteDataSource
+import com.xquare.domain.repository.WebViewRepository
+import javax.inject.Inject
+
+class WebViewRepositoryImpl @Inject constructor(
+    private val authRemoteDataSource: AuthRemoteDataSource,
+    private val authLocalDataSource: AuthLocalDataSource,
+) : WebViewRepository {
+
+    override suspend fun fetchAuthorizationHeaders(): Map<String, String> =
+        mapOf(
+            Pair("Authorization", "Bearer ${authLocalDataSource.fetchToken().accessToken}")
+        )
+
+    override suspend fun refreshAuthorizationHeaders(): Map<String, String> {
+        val refreshToken = authLocalDataSource.fetchToken().refreshToken
+        val newToken = authRemoteDataSource.tokenRefresh(refreshToken)
+        authLocalDataSource.saveToken(newToken)
+        return mapOf(
+            Pair("Authorization", "Bearer ${newToken.accessToken}")
+        )
+    }
+}
