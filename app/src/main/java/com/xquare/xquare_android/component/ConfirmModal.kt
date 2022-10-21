1b04500fd17448b1e7b366faf7c1480ee01fe3c4
shw0471 <shw0471@naver.com>
2022-10-21 13:41:21 +0900

    ⚡️ :: update confirm modal

---
+++ app/src/main/java/com/xquare/xquare_android/component/ConfirmModal.kt
@@ -0,0 +1,51 @@
+package com.xquare.xquare_android.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.semicolon.design.Subtitle4
+import com.semicolon.design.color.primary.black.black
+import com.semicolon.design.color.primary.gray.gray900
+import com.semicolon.design.color.primary.white.white
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun ConfirmModal(
+    message: String,
+    confirmText: String,
+    cancelText: String,
+    onConfirm: () -> Unit,
+    onCancel: () -> Unit,
+) {
+
+    Dialog(
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false
+        ),
+        onDismissRequest = onCancel
+    ) {
+        Column(
+            Modifier
+                .padding(horizontal = 32.dp)
+                .background(color = white, shape = RoundedCornerShape(12.dp))
+                .padding(20.dp),
+        ) {
+            Subtitle4(text = message, color = gray900)
+            Spacer(Modifier.size(16.dp))
+            Row {
+                DefaultModalButton(
+                    Modifier.weight(1f), text = cancelText) { onCancel() }
+                Spacer(Modifier.size(16.dp))
+                PrimaryModalButton(
+                    Modifier.weight(1f), text = confirmText) { onConfirm() }
+            }
+        }
+
+    }
+}
