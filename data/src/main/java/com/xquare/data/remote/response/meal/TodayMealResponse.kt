a125f3d6942916dc1af30efab7f4d859ce18e1d7
shw0471 <shw0471@naver.com>
2022-10-24 18:55:49 +0900

    ⚡️ :: update meals business logic

--- data/src/main/java/com/xquare/data/remote/response/meal/TodayMealResponse.kt
+++ data/src/main/java/com/xquare/data/remote/response/meal/TodayMealResponse.kt
@@ -4,14 +4,20 @@ import com.google.gson.annotations.SerializedName
 import com.xquare.domain.entity.meal.MealEntity
 
 data class TodayMealResponse(
-    @SerializedName("breakfast") val breakfast: List<String>,
-    @SerializedName("lunch") val lunch: List<String>,
-    @SerializedName("dinner") val dinner: List<String>
+    @SerializedName("breakfast") val breakfast: List<String>?,
+    @SerializedName("lunch") val lunch: List<String>?,
+    @SerializedName("dinner") val dinner: List<String>?,
+    @SerializedName("breakfast_kcal") val caloriesOfBreakfast: String?,
+    @SerializedName("lunch_kcal") val caloriesOfLunch: String?,
+    @SerializedName("dinner_kcal") val caloriesOfDinner: String?,
 )
 
 fun TodayMealResponse.toEntity(): MealEntity =
     MealEntity(
-        breakfast = breakfast,
-        lunch = lunch,
-        dinner = dinner
+        breakfast = breakfast ?: listOf(),
+        lunch = lunch ?: listOf(),
+        dinner = dinner ?: listOf(),
+        caloriesOfBreakfast = caloriesOfBreakfast ?: "",
+        caloriesOfLunch = caloriesOfLunch ?: "",
+        caloriesOfDinner = caloriesOfDinner ?: ""
     )
--- domain/src/main/java/com/xquare/domain/entity/meal/MealEntity.kt
+++ domain/src/main/java/com/xquare/domain/entity/meal/MealEntity.kt
@@ -3,5 +3,8 @@ package com.xquare.domain.entity.meal
 data class MealEntity(
     val breakfast: List<String>,
     val lunch: List<String>,
-    val dinner: List<String>
+    val dinner: List<String>,
+    val caloriesOfBreakfast: String,
+    val caloriesOfLunch: String,
+    val caloriesOfDinner: String
 )
