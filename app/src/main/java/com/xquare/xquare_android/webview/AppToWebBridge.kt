61ef96ee745ec5883a2d995ab3e6ca1f51af3353
kimjjunho <kimjjun1007@gmail.com>
2023-02-27 23:44:22 +0900

    ⚡️ :: add bridge app to web

--- app/src/main/java/com/xquare/xquare_android/webview/AppToWebBridge.kt
+++ app/src/main/java/com/xquare/xquare_android/webview/AppToWebBridge.kt
@@ -7,3 +7,28 @@ fun WebView.sendResultOfConfirmModal(
 ) = this.loadUrl(
     "javascript:window.dispatchEvent(new CustomEvent('confirmXBridge', { detail: { success: $isConfirmed }}))"
 )
+
+fun WebView.sendImagesOfPhotoPicker(
+    id: String,
+    photos: List<String>,
+) = this.loadUrl(
+    "javascript:window.dispatchEvent(new CustomEvent('photoPickerXBridge', { detail: { id: $id, photos: $photos }}))"
+)
+
+fun WebView.sendIndexOfActionSheet(
+    id: String,
+    index: Int
+) = this.loadUrl(
+    "javascript:window.dispatchEvent(new CustomEvent('actionSheetXBridge', { detail: { id: $id, index: $index }}))"
+)
+
+fun WebView.sendResultOfRightButton() = this.loadUrl(
+    "javascript:window.dispatchEvent(new CustomEvent('rightButtonTapedXBridge', { detail: {}))"
+)
+
+fun WebView.sendTimeOfTimePicker(
+    id: String,
+    time: String
+) = this.loadUrl(
+    "javascript:window.dispatchEvent(new CustomEvent('timePickerXBridge', { detail: { id: $id, index: $time }))"
+)
