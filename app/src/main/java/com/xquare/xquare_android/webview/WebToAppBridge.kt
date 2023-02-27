d7ee77958ce9b0560aa08318181708f73741f03e
kimjjunho <kimjjun1007@gmail.com>
2023-02-28 00:03:22 +0900

    ⚡️ :: add bridge web to app

--- app/src/main/java/com/xquare/xquare_android/webview/WebToAppBridge.kt
+++ app/src/main/java/com/xquare/xquare_android/webview/WebToAppBridge.kt
@@ -2,6 +2,13 @@ package com.xquare.xquare_android.webview
 
 import android.webkit.JavascriptInterface
 import com.google.gson.Gson
+import com.xquare.xquare_android.webview.data.ActionSheetInfo
+import com.xquare.xquare_android.webview.data.ModalInfo
+import com.xquare.xquare_android.webview.data.NavigateInfo
+import com.xquare.xquare_android.webview.data.PhotoPickerInfo
+import com.xquare.xquare_android.webview.data.RightButtonEnabled
+import com.xquare.xquare_android.webview.data.TimePickerInfo
+import com.xquare.xquare_android.webview.data.WebViewError
 
 class WebToAppBridge(
     val onNavigate: (NavigateInfo) -> Unit = {},
@@ -9,6 +16,10 @@ class WebToAppBridge(
     val onConfirmModal: (ModalInfo) -> Unit = {},
     val onBack: () -> Unit = {},
     val onError: (WebViewError) -> Unit = {},
+    val onPhotoPicker: (PhotoPickerInfo) -> Unit = {},
+    val onActionSheet: (ActionSheetInfo) -> Unit = {},
+    val onTimePicker: (TimePickerInfo) -> Unit = {},
+    val onIsRightButtonEnabled: (RightButtonEnabled) -> Unit = {},
     private val gson: Gson = Gson(),
 ) {
 
@@ -31,4 +42,20 @@ class WebToAppBridge(
     @JavascriptInterface
     fun error(error: String) =
         onError(gson.fromJson(error, WebViewError::class.java))
+
+    @JavascriptInterface
+    fun photoPicker(data: String) =
+        onPhotoPicker(gson.fromJson(data, PhotoPickerInfo::class.java))
+
+    @JavascriptInterface
+    fun actionSheet(data: String) =
+        onActionSheet(gson.fromJson(data, ActionSheetInfo::class.java))
+
+    @JavascriptInterface
+    fun timePicker(data: String) =
+        onTimePicker(gson.fromJson(data, TimePickerInfo::class.java))
+
+    @JavascriptInterface
+    fun isRightButtonEnabled(data: String) =
+        onIsRightButtonEnabled(gson.fromJson(data, RightButtonEnabled::class.java))
 }
