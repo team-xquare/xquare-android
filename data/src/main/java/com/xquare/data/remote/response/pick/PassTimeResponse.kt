1d2fbd14b97848506b06589ea2696df5e6904b90
kimjjunho <kimjjun1007@gmail.com>
2023-03-16 22:31:32 +0900

    ⚡️ :: show pass card end time

--- data/src/main/java/com/xquare/data/remote/response/pick/PassTimeResponse.kt
+++ data/src/main/java/com/xquare/data/remote/response/pick/PassTimeResponse.kt
@@ -13,6 +13,7 @@ fun PassTimeResponse.toEntity() =
     PassTimeEntity(
         user_id = user_id.toString(),
         name = name,
-        end_time = end_time
+        end_time = end_time.substring(0..1) + "시 "
+                    + end_time.substring(3..4) + "분"
     )
 
