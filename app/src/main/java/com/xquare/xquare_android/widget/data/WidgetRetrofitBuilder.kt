e4197cde677134be0c94d1aa2e54069a078e2d03
Android No.1 <yeon8747@naver.com>
2023-03-24 13:52:24 +0900

    ♻️ :: Type refactoring

--- app/src/main/java/com/xquare/xquare_android/widget/data/WidgetRetrofitBuilder.kt
+++ app/src/main/java/com/xquare/xquare_android/widget/data/WidgetRetrofitBuilder.kt
@@ -5,7 +5,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class WidgetRetrofitBuilder {
+object WidgetRetrofitBuilder {
     private val baseURI = "https://api.xquare.app/"
 
     val retrofit = Retrofit.Builder()
