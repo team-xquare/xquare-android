1d6bc92d764a1dc4e8458fe5f23c68ffa79d9be1
Kim Yeon woo <yeon8747@naver.com>
2023-04-18 03:46:35 +0900

    ⚡️ ::	 Repository 추가

--- domain/src/main/java/com/xquare/domain/repository/pick/PickRepository.kt
+++ domain/src/main/java/com/xquare/domain/repository/pick/PickRepository.kt
@@ -3,6 +3,8 @@ package com.xquare.domain.repository.pick
 import com.xquare.domain.entity.pick.ClassPositionEntity
 import com.xquare.domain.entity.pick.PassDataEntity
 import com.xquare.domain.entity.pick.PassTimeEntity
+import com.xquare.domain.entity.pick.TodaySelfStudyTeacherEntity
+import kotlinx.coroutines.flow.Flow
 
 interface PickRepository {
     suspend fun fetchPassTime(): PassTimeEntity
@@ -12,4 +14,6 @@ interface PickRepository {
     suspend fun backToClassRoom()
 
     suspend fun fetchClassPosition(): ClassPositionEntity
+
+    suspend fun TodaySelfStudyTeacher(): Flow<TodaySelfStudyTeacherEntity>
 }
