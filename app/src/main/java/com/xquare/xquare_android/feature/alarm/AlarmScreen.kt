5156df944c4dfce8ab771a65994ca1d18726c8e7
rladmsdh <a01051355543@gmail.com>
2023-05-05 14:20:03 +0900

    ⚡️ ::	알림 불러오기

--- app/src/main/java/com/xquare/xquare_android/feature/alarm/AlarmScreen.kt
+++ app/src/main/java/com/xquare/xquare_android/feature/alarm/AlarmScreen.kt
@@ -1,8 +1,10 @@
 package com.xquare.xquare_android.feature.alarm
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,8 +18,12 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,9 +31,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -35,11 +41,14 @@ import com.semicolon.design.Body3
 import com.semicolon.design.Subtitle4
 import com.semicolon.design.color.primary.gray.gray50
 import com.semicolon.design.color.primary.white.white
+import com.xquare.domain.entity.auth.TokenEntity
 import com.xquare.domain.entity.notification.AlarmEntity
 import com.xquare.xquare_android.R
 import com.xquare.xquare_android.component.Header
 import com.xquare.xquare_android.util.DevicePaddings
 import com.xquare.xquare_android.util.makeToast
+import org.openjdk.tools.javac.parser.Tokens.Token
+import org.openjdk.tools.javac.parser.Tokens.tokensKey
 
 @Composable
 fun AlarmScreen(
@@ -48,7 +57,7 @@ fun AlarmScreen(
     val context = LocalContext.current
     val viewModel: AlarmViewModel = hiltViewModel()
     var alarmList: AlarmEntity?  by remember { mutableStateOf(null) }
-    /*LaunchedEffect(Unit) {
+    LaunchedEffect(Unit) {
         viewModel.fetchAlarmList()
         viewModel.eventFlow.collect {
             when (it) {
@@ -58,7 +67,7 @@ fun AlarmScreen(
                 }
             }
         }
-    }*/
+    }
     Alarm(
         alarmList = alarmList,
         onBackPress = { navController.popBackStack() }
@@ -93,24 +102,18 @@ fun Alarm(
             )
         } else {
             LazyColumn {
-                items(alarmList.notifications) {
-
+                items(alarmList.notifications.count()) {
+                    AlarmItem(alarmList = alarmList.notifications[it])
                 }
             }
         }
     }
 }
-
 @Composable
 fun AlarmItem(
-    title: String,
-    content: String,
-    sentAt: String,
-    isRead: Boolean,
-    iconPainter: Painter,
+    alarmList: AlarmEntity.AlarmDataEntity
 ) {
-    val backgroundColor = if (isRead) white else gray50
-
+    val backgroundColor = if (alarmList.is_read) white else gray50
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -123,14 +126,14 @@ fun AlarmItem(
         ) {
             Spacer(modifier = Modifier.size(19.dp))
             Icon(
-                painter = iconPainter,
+                imageVector = Icons.Default.Check,
                 contentDescription = null,
                 modifier = Modifier.size(20.dp),
             )
             Spacer(modifier = Modifier.width(4.dp))
-            Body3(text = title, color = Color(0xFF616161))
+            Body3(text = alarmList.title, color = Color(0xFF616161))
             Body3(
-                text = sentAt,
+                text = alarmList.send_at,
                 color = Color(0xFF616161),
                 modifier = Modifier
                     .fillMaxWidth()
@@ -138,18 +141,25 @@ fun AlarmItem(
             )
         }
         Spacer(modifier = Modifier.height(4.dp))
-
         Box(
             modifier = Modifier.padding(start = 44.dp, end = 16.dp)
         ) {
             Subtitle4(
-                text = content,
+                text = alarmList.content,
                 color = Color(0xFF212121),
                 lineHeight = 20,
             )
         }
-
         Spacer(modifier = Modifier.height(17.dp))
     }
 }
-
+@Composable
+@Preview
+fun AlarmItemPreview(){
+    AlarmItem(
+        alarmList = AlarmEntity.AlarmDataEntity(
+            "a","a","a","20",
+            false,"a","a"
+        )
+    )
+}
--- data/src/main/java/com/xquare/data/remote/datasource/AlarmRemoteDataSourceImpl.kt
+++ data/src/main/java/com/xquare/data/remote/datasource/AlarmRemoteDataSourceImpl.kt
@@ -1,7 +1,9 @@
 package com.xquare.data.remote.datasource
 
+import android.util.Log
 import com.xquare.data.remote.api.NotificationApi
 import com.xquare.data.remote.response.notification.toEntity
+import com.xquare.data.sendHttpRequest
 import com.xquare.domain.entity.notification.AlarmEntity
 import javax.inject.Inject
 
@@ -9,5 +11,5 @@ class AlarmRemoteDataSourceImpl @Inject constructor(
     private val notificationApi: NotificationApi,
 ): AlarmRemoteDataSource {
     override suspend fun fetchAlarm(): AlarmEntity =
-        notificationApi.fetchAlarm().toEntity()
+        sendHttpRequest(httpRequest = { notificationApi.fetchAlarm().toEntity() })
 }
