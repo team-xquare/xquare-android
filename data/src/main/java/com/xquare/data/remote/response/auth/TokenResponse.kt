4a994f40bf80878785af67dd28d90e526327ef96
Kim Yeon woo <yeon8747@naver.com>
2023-08-17 22:15:38 +0900

    ♻️ :: TokenResponse 수정

--- data/src/main/java/com/xquare/data/remote/response/auth/TokenResponse.kt
+++ data/src/main/java/com/xquare/data/remote/response/auth/TokenResponse.kt
@@ -6,12 +6,16 @@ import org.threeten.bp.LocalDateTime
 
 data class TokenResponse(
     @SerializedName("access_token") val accessToken: String,
+    @SerializedName("access_token_expire_at") val accessTokenExpireAt: String,
     @SerializedName("refresh_token") val refreshToken: String,
+    @SerializedName("refresh_token_expire_at") val refreshTokenExpireAt: String,
     @SerializedName("expire_at") val expirationAt: String
 )
 
 fun TokenResponse.toEntity() = TokenEntity(
     accessToken = accessToken,
+    accessTokenExpireAt = LocalDateTime.parse(accessTokenExpireAt),
     refreshToken = refreshToken,
+    refreshTokenExpireAt = LocalDateTime.parse(refreshTokenExpireAt),
     expirationAt = LocalDateTime.parse(expirationAt)
 )
