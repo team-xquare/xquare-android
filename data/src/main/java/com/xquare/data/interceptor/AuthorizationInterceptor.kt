87a705fa960f0f641ae4a6e4978d6ec4ca3acc42
rladmsdh <a01051355543@gmail.com>
2023-08-24 16:05:45 +0900

    🐛 :: 자동로그인 버그 고침

--- data/src/main/java/com/xquare/data/interceptor/AuthorizationInterceptor.kt
+++ data/src/main/java/com/xquare/data/interceptor/AuthorizationInterceptor.kt
@@ -41,7 +41,7 @@ class AuthorizationInterceptor @Inject constructor(
             val refreshToken = runBlocking { authPreference.fetchRefreshToken() }
 
             val tokenRefreshRequest = Request.Builder()
-                .url("https://api.xquare.app/users/login")
+                .url("https://prod-server.xquare.app/users/login")
                 .put("".toRequestBody("application/json".toMediaTypeOrNull()))
                 .addHeader("Refresh-Token", "Bearer $refreshToken")
                 .build()
