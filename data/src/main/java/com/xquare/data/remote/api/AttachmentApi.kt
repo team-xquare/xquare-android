df88e23f4a36766feca8a8311701e46d5e5f7239
junsuPark <krpjs0508@gmail.com>
2023-08-29 23:50:34 +0900

    ♻️ :: bucket name 제거

--- data/src/main/java/com/xquare/data/remote/api/AttachmentApi.kt
+++ data/src/main/java/com/xquare/data/remote/api/AttachmentApi.kt
@@ -1,17 +1,14 @@
 package com.xquare.data.remote.api
 
-import com.xquare.data.remote.request.FileRequest
 import com.xquare.data.remote.response.attachment.FileResponse
 import okhttp3.MultipartBody
-import retrofit2.http.Body
-import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 
 interface AttachmentApi {
     @Multipart
-    @POST("attachment?bucketName=xquare")
+    @POST("attachment")
     suspend fun uploadFile(
         @Part files: MultipartBody.Part
     ): FileResponse
