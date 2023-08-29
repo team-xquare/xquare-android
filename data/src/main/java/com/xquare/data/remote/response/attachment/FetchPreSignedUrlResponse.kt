a7f26867500c3d8528cb96c9a806f9da530a1c70
junsuPark <krpjs0508@gmail.com>
2023-08-30 00:29:06 +0900

    ♻️ :: todo

--- data/src/main/java/com/xquare/data/remote/response/attachment/FetchPreSignedUrlResponse.kt
+++ data/src/main/java/com/xquare/data/remote/response/attachment/FetchPreSignedUrlResponse.kt
@@ -2,6 +2,7 @@ package com.xquare.data.remote.response.attachment
 
 import com.google.gson.annotations.SerializedName
 
+// todo response 수정 고민
 data class FetchPreSignedUrlResponse(
     @SerializedName("url") val url: String,
     @SerializedName("presigned_url") val preSignedUrl: String,
