c02fcd9636dfee37bbffcc5411081a95acbc3371
junsuPark <krpjs0508@gmail.com>
2023-08-30 14:46:47 +0900

    ♻️ :: FetchPreSignedUrlResponse Serial name 수정

--- data/src/main/java/com/xquare/data/remote/response/attachment/FetchPreSignedUrlResponse.kt
+++ data/src/main/java/com/xquare/data/remote/response/attachment/FetchPreSignedUrlResponse.kt
@@ -4,8 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 // todo response 수정 고민
 data class FetchPreSignedUrlResponse(
-    @SerializedName("url") val url: String,
-    @SerializedName("presigned_url") val preSignedUrl: String,
-    @SerializedName("original_filename") val originalFilename: String,
-    @SerializedName("content_type") val contentType: String,
-)
+    @SerializedName("presigned_urls") val responses: List<ImageFileResponse>,
+) {
+    data class ImageFileResponse(
+        @SerializedName("url") val url: String,
+        @SerializedName("presigned_url") val preSignedUrl: String,
+        @SerializedName("original_filename") val originalFilename: String,
+        @SerializedName("content_type") val contentType: String,
+    )
+}
