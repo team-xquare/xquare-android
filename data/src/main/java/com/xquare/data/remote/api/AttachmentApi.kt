99098ff436627b1062e7b7f45bdf4ce479569080
junsuPark <krpjs0508@gmail.com>
2023-08-30 18:24:34 +0900

    ♻️ :: attachment api Body annotation

--- data/src/main/java/com/xquare/data/remote/api/AttachmentApi.kt
+++ data/src/main/java/com/xquare/data/remote/api/AttachmentApi.kt
@@ -2,6 +2,7 @@ package com.xquare.data.remote.api
 
 import com.xquare.data.remote.request.attachment.FetchPreSignedUrlRequest
 import com.xquare.data.remote.response.attachment.FetchPreSignedUrlResponse
+import retrofit2.http.Body
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 
@@ -9,6 +10,6 @@ interface AttachmentApi {
     @Multipart
     @POST("attachment")
     suspend fun fetchPreSignedUrl(
-        fetchPreSignedUrlRequest: FetchPreSignedUrlRequest,
+        @Body fetchPreSignedUrlRequest: FetchPreSignedUrlRequest,
     ): FetchPreSignedUrlResponse
 }
