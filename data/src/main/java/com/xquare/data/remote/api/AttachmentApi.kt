070c64ba416bfb48ef3282c275159e7c291800c1
junsuPark <krpjs0508@gmail.com>
2023-08-31 17:32:14 +0900

    ♻️ :: optimize import

--- data/src/main/java/com/xquare/data/remote/api/AttachmentApi.kt
+++ data/src/main/java/com/xquare/data/remote/api/AttachmentApi.kt
@@ -3,7 +3,6 @@ package com.xquare.data.remote.api
 import com.xquare.data.remote.request.attachment.FetchPreSignedUrlRequest
 import com.xquare.data.remote.response.attachment.FetchPreSignedUrlResponse
 import retrofit2.http.Body
-import retrofit2.http.Multipart
 import retrofit2.http.POST
 
 interface AttachmentApi {
