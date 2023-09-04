ab7b2db7f8d4a83c3ed0db34c3bb167b6b114103
junsuPark <krpjs0508@gmail.com>
2023-09-05 08:01:38 +0900

    ⚡️ :: FileUploadManager 구현

--- data/src/main/java/com/xquare/data/remote/FileUploadManager.kt
+++ data/src/main/java/com/xquare/data/remote/FileUploadManager.kt
@@ -1,12 +1,16 @@
 package com.xquare.data.remote
 
-import com.xquare.data.local.preference.AuthPreference
+import android.webkit.MimeTypeMap
+import com.xquare.data.remote.ContentType.Companion.mediaType
 import com.xquare.data.remote.api.AttachmentApi
 import com.xquare.data.remote.request.attachment.FetchPreSignedUrlRequest
+import com.xquare.domain.entity.attachment.FileEntity
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
+import kotlinx.coroutines.launch
+import okhttp3.Interceptor
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.asRequestBody
@@ -14,65 +18,77 @@ import java.io.File
 
 class FileUploadManager(
     private val attachmentApi: AttachmentApi,
+    private val httpLoggingInterceptor: Interceptor,
 ) {
-    private val httpClient by lazy { OkHttpClient() }
-    private val accessToken: String
-        get() = runBlocking(Dispatchers.IO) { authPreference.fetchAccessToken() }
+    private val httpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
 
-    suspend fun uploadFile(vararg files: File) {
-        require(files.all { file -> file.length() < 10 * 1_000_000 })
+    suspend fun uploadFile(vararg files: File): FileEntity {
+        return try {
+            require(files.all { file -> file.length() < 10 * 1_000_000 })
 
-        val request1 = files.toList().toRequest()
-        val response1 = attachmentApi.fetchPreSignedUrl(request1)
-        val requests = request1.requests
+            val request1 = files.toList().toRequest()
 
-        // 네이밍 죄송합니다
-        response1.responses.forEachIndexed { index, response2 ->
-            requests[index].let { request2 ->
-                val file = files[index]
-                val body = MultipartBody.Builder().setType(MultipartBody.FORM)
-                    .addFormDataPart(
-                        name = "file",
-                        filename = file.name,
-                        body = file.asRequestBody(
-                            request2.contentType.toMediaTypeOrNull(),
-                        ),
-                    ).build()
-                val request = Request.Builder()
-                    .url(response2.preSignedUrl)
-                    .method("PUT", body)
-                    .build()
-                httpClient.newCall(request)
-            }
+            val response =
+                attachmentApi.fetchPreSignedUrl(request1).responses.onEachIndexed { index, response ->
+                    request1.requests[index].let { request ->
+                        val file = files[index]
+                        val type = file.mediaType
+                        val newRequest = Request.Builder()
+                            .url(response.preSignedUrl)
+                            .put(file.asRequestBody(type))
+                            .build()
+                        CoroutineScope(Dispatchers.IO).launch {
+                            httpClient.newCall(newRequest).execute()
+                        }
+                    }
+                }
+
+            FileEntity(fileUrls = response.map { it.url })
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw e
         }
     }
 }
 
 enum class ContentType(val value: String) {
-    JPEG("image/jpeg"), JPG("image/jpg"), JPE("image/jpe"), GIF("image/gif"), WEBP("image/webp");
+    JPEG("image/jpeg"),
+    JPG("image/jpg"),
+    JPE("image/jpe"),
+    GIF("image/gif"),
+    WEBP("image/webp"),
+    PNG("image/png"),
+    HEIC("image/heic");
 
     companion object {
-        fun fromString(value: String): ContentType {
-            return when (value) {
-                "jpeg" -> JPEG
-                "jpg" -> JPG
-                "jpe" -> JPE
-                "gif" -> GIF
-                "webp" -> WEBP
-                else -> throw IllegalArgumentException()
+        val File.mediaType: MediaType
+            get() {
+                val type = fromString(MimeTypeMap.getFileExtensionFromUrl(this.path)).value
+                return type.toMediaType()
             }
+
+        fun fromString(value: String): ContentType = when (value) {
+            "jpeg" -> JPEG
+            "jpg" -> JPG
+            "jpe" -> JPE
+            "gif" -> GIF
+            "webp" -> WEBP
+            "png" -> PNG
+            "heic" -> HEIC
+            else -> throw IllegalArgumentException()
         }
     }
 }
 
 fun File.toRequest(): FetchPreSignedUrlRequest.ImageFileRequest {
-    val (_, contentType) = this.name.split('.')
-
     return FetchPreSignedUrlRequest.ImageFileRequest(
         originalFilename = this.name,
-        contentType = ContentType.fromString(contentType).value,
-        // todo 수정
-        fileSize = 10
+        contentType = ContentType.fromString(MimeTypeMap.getFileExtensionFromUrl(this.path)).value,
+        fileSize = this.length(),
     )
 }
 
