1a630143e183daa1cab05bc1ac711a0135541ec8
junsuPark <krpjs0508@gmail.com>
2023-09-05 14:06:52 +0900

    ⚡️ :: FileDataSource 수정

--- data/src/main/java/com/xquare/data/remote/datasource/AttachmentRemoteDataSource.kt
+++ data/src/main/java/com/xquare/data/remote/datasource/AttachmentRemoteDataSource.kt
@@ -1,8 +1,9 @@
 package com.xquare.data.remote.datasource
 
 import com.xquare.domain.entity.attachment.FileEntity
+import kotlinx.coroutines.flow.Flow
 import java.io.File
 
 interface AttachmentRemoteDataSource {
-    suspend fun uploadFile(file: File): FileEntity
+    suspend fun uploadFile(file: File): Flow<FileEntity>
 }
