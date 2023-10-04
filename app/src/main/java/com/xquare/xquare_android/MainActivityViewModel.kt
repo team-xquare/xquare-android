8f92456414343bbf4e3a56f0b4d257e8f9258b16
junsuPark <krpjs0508@gmail.com>
2023-10-04 18:48:38 +0900

    ♻️ :: MainActivityViewModel github registration flag

--- app/src/main/java/com/xquare/xquare_android/MainActivityViewModel.kt
+++ app/src/main/java/com/xquare/xquare_android/MainActivityViewModel.kt
@@ -1,33 +1,40 @@
 package com.xquare.xquare_android
 
+import androidx.lifecycle.viewModelScope
 import com.xquare.domain.entity.github.GithubOAuthEntity
 import com.xquare.domain.usecase.github.FetchGithubOAuthCheckUseCase
 import com.xquare.domain.usecase.github.FetchGithubOAuthUseCase
 import com.xquare.xquare_android.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
     private val fetchGithubOAuthUseCase: FetchGithubOAuthUseCase,
-    private val fetchGithubOAuthCheckUseCase: FetchGithubOAuthCheckUseCase
+    private val fetchGithubOAuthCheckUseCase: FetchGithubOAuthCheckUseCase,
 ) : BaseViewModel<MainActivityViewModel.Event>() {
 
+    private var called = false
     fun registerGithubUser(githubOAuthEntity: GithubOAuthEntity) {
-        execute(
-            job = {
-                val connected = fetchGithubOAuthCheckUseCase.execute(Unit).is_connected
+        if (!called) {
+            called = true
+            viewModelScope.launch(Dispatchers.IO) {
+                runCatching {
+                    val connected = fetchGithubOAuthCheckUseCase.execute(Unit).is_connected
 
-                if (!connected) {
-                    fetchGithubOAuthUseCase.execute(githubOAuthEntity)
+                    if (!connected) {
+                        fetchGithubOAuthUseCase.execute(githubOAuthEntity)
+                    }
+                }.onSuccess {
+                    emitEvent(Event.OAuthSuccess)
+                }.onFailure {
+                    emitEvent(Event.OAuthFailure)
+                    it.printStackTrace()
                 }
-            },
-            onSuccess = { emitEvent(Event.OAuthSuccess) },
-            onFailure = { emitEvent(Event.OAuthFailure) }
-        )
+            }
+        }
     }
 
     sealed class Event {
